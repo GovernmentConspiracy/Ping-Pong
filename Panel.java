4dc8bd2b4cccfa7653ad61c38b3d69a595259e46
Feguette <liang.j4son@gmail.com>
2017-05-18 23:05:44 -0700

    Final Product

---
+++ Panel.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Panel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public interface Panel
+{
+    Rectangle getWall();
+    double[] getReflector();
+}
